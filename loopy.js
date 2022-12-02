469f32b5eb2076adc932165295ed960954438795
Diego Mendoza <im@mendozadiego.com>
2022-12-01 21:52:28 -0500

    Loopy exercise

---
+++ loopy.js
@@ -0,0 +1,13 @@
+// Loop that prints from 100 to 200
+
+for (let loop = 100; loop <= 200; loop++){
+  if (loop % 3 === 0 && loop % 4 === 0){
+    console.log("LoopyLighthouse")
+  }else if (loop % 3 === 0){
+    console.log("Loopy")
+  }else if(loop % 4 === 0){
+    console.log("Lighthouse")
+  }else{
+    console.log(loop)
+  }
+}
