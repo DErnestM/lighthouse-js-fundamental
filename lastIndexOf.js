08822f986749377b25c3fff7b2eb252e1b318b63
Diego Mendoza <im@mendozadiego.com>
2022-12-07 22:59:26 -0500

    Looping arrays- added lastIndexOf.js

---
+++ lastIndexOf.js
@@ -0,0 +1,17 @@
+function lastIndexOf(arr, value) {
+  for (let index = arr.length - 1; index >= 0; index--) {
+    if (arr[index] === value) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+
+
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
