cac425e49efa25ba47585d0ef17294bfa21e7b23
Diego Mendoza <im@mendozadiego.com>
2022-12-08 13:44:17 -0500

    SmartGarbage.js added - playing with objects

---
+++ smartGarbage.js
@@ -0,0 +1,15 @@
+const smartGarbage= function (trash, bins) {
+  if (trash === "waste") {
+    bins["waste"]++
+  }
+  if (trash === "recycling") {
+    bins["recycling"]++
+  }
+  if (trash === "compost") {
+    bins["compost"]++
+  }
+  return bins
+}
+
+
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }))
