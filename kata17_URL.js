2a5ce52b20963bd1b31ec934ef21cb3425fc0ade
Diego Mendoza <im@mendozadiego.com>
2022-12-25 16:23:56 -0500

    kata17_url added

---
+++ kata17_URL.js
@@ -0,0 +1,40 @@
+/*
+Create a function named urlDecode that will receive a URL encoded string, and return the a JavaScript object that represents that data.
+*/
+
+const urlDecode = function(text) {
+
+  let values = []
+  let output = {}
+
+  if(text.includes("&")){
+    
+    values = text.split("&")
+    
+    values.forEach(function(word){
+    word = word.replace(/%20/g," ")
+    let key = word.slice(0, word.indexOf("="))
+    let value = word.slice(word.indexOf("=")+1)
+    output[key] = value
+    })
+  }else{
+    text = text.replace(/%20/g," ")
+    let key = text.slice(0, text.indexOf("="))
+    let value = text.slice(text.indexOf("=")+1)
+    output[key] = value
+    }
+  return output
+  }
+;
+
+console.log(urlDecode("duck=rubber"));
+console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
+
+/*
+{duck: "rubber"}
+{bootcamp: "Lighthouse Labs"}
+{city: "Vancouver", weather: "lots of rain"}
+"lots of rain"
+*/
